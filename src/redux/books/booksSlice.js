744c4e4c757bd20c73d0abea8311a874c8086467
kazaneza <kazanezagentil@gmail.com>
2023-03-18 20:19:14 +0100

    made changes

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,40 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
 };
 
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, actions) => {
-      const bookData = actions.payload;
-      state.books.push(bookData);
+    addBook: (state, action) => {
+      const bookData = action.payload;
+      state.books = [...state.books, bookData];
     },
 
-    removeBook: (state, actions) => {
-      const idOfBookToRemove = actions.payload;
-      // eslint-disable-next-line no-param-reassign
+    removeBook: (state, action) => {
+      const idOfBookToRemove = action.payload;
       state.books = state.books.filter(
         (book) => book.item_id !== idOfBookToRemove,
       );
